82f1a5f20b8fcaa66725b4170946005ca7130fb8
daniel-browning <daniel.d.browning@gmail.com>
2018-10-18 14:45:19 -0500

    user input exercise;

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -1,22 +1,57 @@
 package com.codeup.adlister.dao;
-import com.codeup.adlister.models.Config;
 
-public class DaoFactory {
-    private static Ads adsDao;
-    private static Users usersDao;
-    private static Config config = new Config();
+import com.codeup.adlister.models.Ad;
 
-    public static Ads getAdsDao() {
-        if (adsDao == null) {
-            adsDao = new MySQLAdsDao(config);
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAdsDao implements Ads {
+    private List<Ad> ads;
+
+    public List<Ad> all() {
+        if (ads == null) {
+            ads = generateAds();
         }
-        return adsDao;
+        return ads;
     }
 
-    public static Users getUsersDao() {
-        if (usersDao == null) {
-            usersDao = new MySQLUsersDao(config);
+    public Long insert(Ad ad) {
+        // make sure we have ads
+        if (ads == null) {
+            ads = generateAds();
         }
-        return usersDao;
+
+        ad.setId((long) ads.size());
+        ads.add(ad);
+        return ad.getId();
+    }
+
+    private List<Ad> generateAds() {
+        List<Ad> ads = new ArrayList<>();
+        ads.add(new Ad(
+                1,
+                1,
+                "Television for sale",
+                "Somebody please buy this tv"
+        ));
+        ads.add(new Ad(
+                2,
+                1,
+                "Lululemon Men\'s Apparel",
+                "Old apparel from an ex I\'d rather sell than burn."
+        ));
+        ads.add(new Ad(
+                3,
+                2,
+                "Personal Assistant needed",
+                "Fix my life, I need help!"
+        ));
+        ads.add(new Ad(
+                4,
+                2,
+                "Apartment for rent",
+                "Great condition, only a few roaches."
+        ));
+        return ads;
     }
 }
